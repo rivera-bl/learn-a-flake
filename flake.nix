6a666b569706e7cece7ea66648364273a6473583
rivera-bl <rivera.pablo@gmail.com>
2022-07-04 10:12:02 -0400

    Refactor with flake-utils

--- flake.nix
+++ flake.nix
@@ -1,81 +1,34 @@
+/* TODO: name the pkg and export explicitily as the default */
+/* TODO: explicit the defaultapp */
+/* this is a good example of the flake of an application using go, notheless it is more important to us, to apply a flake to a terminal tool and the nixos system*/
 {
   description = "A simple Go package";
 
-  # Nixpkgs / NixOS version to use.
-  inputs.nixpkgs.url = "nixpkgs/nixos-21.11";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-21.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { self, nixpkgs }:
-    let
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let 
+        lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+        version = builtins.substring 0 8 lastModifiedDate;
+        pkgs =  import nixpkgs { inherit system; };
+      in
+      {
 
-      # to work with older version of flakes
-      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+        defaultPackage = pkgs.buildGoModule {
+          pname = "go-hello";
+          inherit version;
+          src = ./.;
 
-      # Generate a user-friendly version number.
-      version = builtins.substring 0 8 lastModifiedDate;
-
-      # System types to support.
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
-
-      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-
-      # Nixpkgs instantiated for supported system types.
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
-
-    in
-    {
-
-      # Provide some binary packages for selected system types.
-      packages = forAllSystems (system:
-        let
-          pkgs = nixpkgsFor.${system};
-        in
-        rec {
-          go-hello = pkgs.buildGoModule {
-            pname = "go-hello";
-            inherit version;
-            # In 'nix develop', we don't need a copy of the source tree
-            # in the Nix store.
-            src = ./.;
-
-            # This hash locks the dependencies of this package. It is
-            # necessary because of how Go requires network access to resolve
-            # VCS.  See https://www.tweag.io/blog/2021-03-04-gomod2nix/ for
-            # details. Normally one can build with a fake sha256 and rely on native Go
-            # mechanisms to tell you what the hash should be or determine what
-            # it should be "out-of-band" with other tooling (eg. gomod2nix).
-            # To begin with it is recommended to set this, but one must
-            # remeber to bump this hash when your dependencies change.
-            #vendorSha256 = pkgs.lib.fakeSha256;
-
-            vendorSha256 = "sha256-pQpattmS9VmO3ZIQUFn66az8GSmB4IvYhTTCFn6SUmo=";
-          };
-          default = go-hello;
-      });
-
-      apps = forAllSystems (system: rec {
-        go-hello = {
-            type = "app";
-            program = "${self.packages.${system}.default}/bin/go-hello";
+          vendorSha256 = "sha256-pQpattmS9VmO3ZIQUFn66az8GSmB4IvYhTTCFn6SUmo=";
         };
-        default = go-hello;
-      });
 
-      defaultApp = forAllSystems (system: self.apps.${system}.default);
-
-      devShells = forAllSystems (system:
-        let pkgs = nixpkgsFor.${system};
-        in {
-          default = pkgs.mkShell {
-            buildInputs = with pkgs; [ go gopls gotools go-tools ];
-          };
-        });
-
-      devShell = forAllSystems (system: self.devShells.${system}.default);
-
-      # The default package for 'nix build'. This makes sense if the
-      # flake provides only one package or there is a clear "main"
-      # package.
-      defaultPackage = forAllSystems (system: self.packages.${system}.go-hello);
-    };
+        devShell = with pkgs; # avoid writing pkgs.mkShell, pkgs.go ..
+          mkShell {
+            buildInputs = [ go gopls gotools go-tools ];
+         };
+    });
 }
